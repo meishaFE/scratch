90457723c05badac639bc51a6a685105953bcb46
Troy <troy@meishakeji.com>
2019-04-17 14:41:21 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { compose } from 'redux';
+import GUI from './containers/gui.jsx';
+import AppStateHOC from './lib/app-state-hoc.jsx';
+import HashParserHOC from './lib/hash-parser-hoc.jsx';
+import TitledHOC from './lib/titled-hoc.jsx';
+
+const Scratch = compose(
+  AppStateHOC,
+  HashParserHOC,
+  TitledHOC
+)(GUI);
+
+export default Scratch;
